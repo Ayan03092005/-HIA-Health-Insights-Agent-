f27b0d8fc005e39276bf56efc1c6ceaf994f8bab
Ayan Chattopadhyay <ayanchattopadhyay05@gmail.com>
2026-03-01 10:13:52 +0530

    change in sidebar
    Removed footer from sidebar.

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from auth.session_manager import SessionManager
-from components.footer import show_footer
 from config.app_config import ANALYSIS_DAILY_LIMIT
 
 def show_sidebar():
@@ -56,9 +55,6 @@ def show_sidebar():
         if st.button("Logout", use_container_width=True):
             SessionManager.logout()
             st.rerun()
-        
-        # Add footer to sidebar
-        show_footer(in_sidebar=True)
 
 def show_session_list():
     if st.session_state.user and 'id' in st.session_state.user:
